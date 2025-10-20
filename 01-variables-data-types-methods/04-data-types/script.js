89e7443b25528e9a862b40d63e3d12beb7b3d3de
TheQueCode <thequecode@gmail.com>
2025-10-20 13:28:06 -0500

    Section 01 - Lesson 04 - Data Types

---
+++ 01-variables-data-types-methods/04-data-types/script.js
@@ -0,0 +1,42 @@
+// Primitive Data Types
+
+// String
+const firstName = 'Sara';
+
+// Number
+const age = 30;
+const temp = 98.6;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 9007199254740991n;
+
+// Reference Types (Objects)
+
+// Object literals
+const person = {
+  name: 'Brad',
+};
+
+// Arrays
+const numbers = [1,2,3,4];
+
+// Functions
+function sayHello(){
+  console.log('Hello');
+}
+
+const output = sayHello;
+console.log(output, typeof output);
