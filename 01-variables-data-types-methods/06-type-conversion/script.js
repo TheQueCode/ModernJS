006ca66ca561d4bb25244c78b5310082c65c230d
TheQueCode <thequecode@gmail.com>
2025-10-20 17:57:21 -0500

    Section 01 - Lesson 06 - Type Conversion

---
+++ 01-variables-data-types-methods/06-type-conversion/script.js
@@ -0,0 +1,34 @@
+let amount = 1;
+
+// Convert string to number
+
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+
+// Convert number to string
+
+// amount = amount.toString();
+// amount = String(amount);
+
+
+// Convert string to decimal
+
+// amount = parseFloat(amount);
+
+
+// Convert number to Boolean
+
+// amount = Boolean(amount); //0 is false; everything else is true
+
+
+// NaN
+
+// amount = parseInt('hello');
+// Math.sqrt(-1);
+// 1 + NaN
+// undefined + undefined
+// 'poo' / 3
+
+console.log(amount, typeof amount);
