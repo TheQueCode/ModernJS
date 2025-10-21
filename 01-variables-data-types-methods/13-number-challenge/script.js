ea44f14987164bf569efa9908e41f37db830c915
TheQueCode <thequecode@gmail.com>
2025-10-20 21:04:13 -0500

    Section 01 - Lesson 13 - Number Challenge

---
+++ 01-variables-data-types-methods/13-number-challenge/script.js
@@ -0,0 +1,10 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+console.log(sumOutput = `${x} + ${y} = ${x + y}`);
+console.log(differentOutput = `${x} - ${y} = ${x - y}`);
+console.log(productOutput = `${x} * ${y} = ${x * y}`);
+console.log(quotientOutput = `${x} / ${y} = ${x / y}`);
+console.log(rmOutput = `${x} % ${y} = ${x % y}`);
+
+
