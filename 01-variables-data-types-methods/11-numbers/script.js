5f87da2b10ab671cd4898dce6032197495d4f3d4
TheQueCode <thequecode@gmail.com>
2025-10-20 20:41:49 -0500

    Section 01 - Lesson 11 - Numbers

---
+++ 01-variables-data-types-methods/11-numbers/script.js
@@ -0,0 +1,30 @@
+let x;
+
+const num = 5;
+
+x = num.toString();
+
+x = num.toString().length; //length of a number
+
+x = num.toFixed(2); // 5.00
+
+x = num.toPrecision(2); // 5.0
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('en-US');
+
+x = num.valueOf(x); // 5
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+
+
+
+
+
+
+
+
+console.log(x);
